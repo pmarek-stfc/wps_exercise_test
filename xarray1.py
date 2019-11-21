f3e32c219eb7bba051c7f973809a99eadc98d0ba
Peter Marek <peter.marek@stfc.ac.uk>
2019-11-21 12:10:54 +0000

    add temporal average

--- xarray1.py
+++ xarray1.py
@@ -3,14 +3,16 @@ import glob
 import time
 import os
 import cftime
+from pathlib import Path
 
 
 start_time = time.time()
 max_lon, min_lon, max_lat, min_lat = 120, 80, 60, -60
 
-files_path = '/home/pmarek/badc/cmip5/data/cmip5/output1/MOHC/HadCM3/rcp45/mon/atmos/Amon/r1i1p1/latest/tas'
-file_path_absolute = os.path.join(os.environ.get('HOME'), files_path)
-files = glob.glob(file_path_absolute + '/*.nc')
+files_path = 'badc/cmip5/data/cmip5/output1/MOHC/HadCM3/rcp45/mon/atmos/Amon/r1i1p1/latest/tas'
+absolute_path = os.path.join(str(Path.home()), files_path)
+files = glob.glob(absolute_path + '/*.nc')
+
 #
 
 if __name__=='__main__':
@@ -19,10 +21,12 @@ if __name__=='__main__':
     files_to_open = couple_subset(files)
     new_dataset = open_mfdatasets(files_to_open)
 
+
     x = new_dataset['tas'].sel(time=slice(d1, d2), lon=slice(min_lon, max_lon), lat=slice(min_lat, max_lat))
-    y = x.mean
-    print(dir(y))
+    mean_array = x.mean()
+    print(mean_array.values)
     print(f'took {time.time() - start_time} seconds')
+
 
 
 
