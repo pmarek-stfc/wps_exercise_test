f5047aec0b4cda5be10e7eac868c9d25b2a3e669
Peter Marek <peter.marek@stfc.ac.uk>
2019-11-26 14:31:54 +0000

    changes for get_years

--- x.py
+++ x.py
@@ -3,35 +3,75 @@ from SortedSet.sorted_set import SortedSet
 from itertools import combinations
 
 
-def get_years(dataset):
-    open_dataset = xr.open_dataset(dataset)
-    first_year_in_dataset = open_dataset.variables['time_bnds'][0].data[0].strftime('%Y')
-    print(first_year_in_dataset)
-    if first_year_in_dataset > '2019':
-        return None
-    else:
-        df = open_dataset.to_dataframe()
-        dates = df['time_bnds']
-        desired_years_set = set()
-        years_found_set = set()
-        desired_years = ['2010', '2011', '2012','2013', '2014',
-                         '2015', '2016', '2017', '2018', '2019']
+def _get_first_year(ds):
+    first_year = ds.variables['time_bnds'][0].data[0].strftime('%Y')
+    return first_year
 
-        for i in dates:
-            year = i.strftime('%Y')
-            years_found_set.add(year)
-            if year in desired_years:
-                desired_years_set.add(year)
-        return SortedSet(years_found_set), SortedSet(desired_years_set)
+def _check_for_years(ds):
+    """
+    Get through all years in a dataset
+    :param ds: a netCDF4 file
+    :return: set of years (should return years 2010 to 2019)
+    """
+    # convert to Pandas data frame
+    df = ds.to_dataframe()
+    dates = df['time_bnds']
+    desired_years_set = set()
+    # years_found_set = set()
+
+    for i in dates:
+        year = i.strftime('%Y')
+        # years_found_set.add(year)
+        if str(2009) < year < str(2020):
+            desired_years_set.add(year)
+    return desired_years_set
+
+
+def get_years(dataset):
+    """
+        :param dataset: netCDF4 file or files
+        :return: None: if the dataset does not include years 2010 to 2019
+                 valid_files: a list of files which contain years 2010 to 2019
+    """
+    valid_files = []
+    if isinstance(dataset, str):
+        with xr.open_dataset(dataset) as ds:
+            year = _get_first_year(ds)
+            print(year)
+            if year > '2019':
+                return None
+            desired_years = _check_for_years(ds)
+            if len(desired_years) == 10:
+                valid_files.append(dataset)
+
+
+    else:
+        for file in dataset:
+            with xr.open_dataset(file, autoclose=True) as ds:
+                year = _get_first_year(ds)
+                print(year)
+                if year > '2019':
+                    continue
+                desired_years = _check_for_years(ds)
+                if len(desired_years) == 10:
+                    valid_files.append(file)
+    return valid_files
 
 
 def couple_subset(files):
-    """ return list of tuples of all subsets of length couple """
+    """ Returns all possible couples of files """
     couple = 2
     return list(combinations(files, couple))
 
 
 def open_mfdatasets(files_to_open):
+    """
+        Test if a couple of data sets can be aggregated together
+        and add them into a set if so
+
+        :param files_to_open: found netCDF4 files
+        :return: opened netCDF data sets using `open_mfdataset`
+    """
     correct_dataset_couples = []
     for combination in files_to_open:
         try:
@@ -43,5 +83,4 @@ def open_mfdatasets(files_to_open):
     correct_dataset = set()
     for elements in correct_dataset_couples:
         correct_dataset.update(elements)
-    print(correct_dataset)
-    return xr.open_mfdataset(correct_dataset, concat_dim='time', combine='by_coords')
+    return xr.open_mfdataset(correct_dataset)
--- xarray1.py
+++ xarray1.py
@@ -9,21 +9,26 @@ from pathlib import Path
 start_time = time.time()
 max_lon, min_lon, max_lat, min_lat = 120, 80, 60, -60
 
+# one_file = 'tas_Amon_HadGEM2-ES_rcp45_r1i1p1_203012-205511.nc'
 files_path = 'badc/cmip5/data/cmip5/output1/MOHC/HadCM3/rcp45/mon/atmos/Amon/r1i1p1/latest/tas'
 absolute_path = os.path.join(str(Path.home()), files_path)
 files = glob.glob(absolute_path + '/*.nc')
-
-d1 = cftime.Datetime360Day(2010, 1, 1)
-d2 = cftime.Datetime360Day(2020, 1, 1)
+# #
+# d1 = cftime.Datetime360Day(2010, 1, 1)
+# d2 = cftime.Datetime360Day(2020, 1, 1)
 
 
 if __name__=='__main__':
-    files_to_open = couple_subset(files)
-    new_dataset = open_mfdatasets(files_to_open)
+    print(get_years(files))
 
-    x = new_dataset['tas'].sel(time=slice(d1, d2), lon=slice(min_lon, max_lon), lat=slice(min_lat, max_lat))
-    mean_array = x.mean()
-    print(mean_array.values)
+    # files_to_open = couple_subset(files)
+    # new_dataset = open_mfdatasets(files_to_open)
+    #
+    # x = new_dataset['tas'].sel(time=slice(d1, d2), lon=slice(min_lon, max_lon), lat=slice(min_lat, max_lat))
+    # # calculate temporal average across the time axis only
+    # mean_array = x.mean(dim='time')
+    # for i in mean_array:
+    #     print(i)
 
     print(f'took {time.time() - start_time} seconds')
 
