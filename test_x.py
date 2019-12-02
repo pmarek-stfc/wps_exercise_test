fixed bugs for calculation

test_x.py
@@ -2,38 +2,89 @@ import unittest
 import x
 import glob
 import os
+from x import open_mfdatasets
+import cftime
+from pathlib import Path
+
+max_lon, min_lon, max_lat, min_lat = 120, 80, 60, -60
+d1 = cftime.Datetime360Day(2010, 1, 1)
+d2 = cftime.Datetime360Day(2020, 1, 1)
+
+def calc_temporal_avg_one():
+    """ calculate temporal average across the time axis only
+        FOR ONE FILE """
+    files_path = 'xarray/tas_Amon_HadGEM2-CC_rcp45_r1i1p1_200512-203011.nc'
+    absolute_path = os.path.join(str(Path.home()), files_path)
+    y = open_mfdatasets(absolute_path)
+    x = y.sel(time=slice(d1, d2), lon=slice(min_lon, max_lon), lat=slice(min_lat, max_lat))
+    # calculate temporal average across the time axis only
+    mean_array = x.mean(dim='time')
+    # print(mean_array)
+    return mean_array
+
+
+def calc_temporal_avg_multiple():
+    """ calculate temporal average across the time axis only
+        FOR MORE FILES"""
+    files_path = 'xarray'
+    absolute_path = os.path.join(str(Path.home()), files_path)
+    files = glob.glob(absolute_path + '/*.nc')
+    y = open_mfdatasets(files)
+    x = y.sel(time=slice(d1, d2), lon=slice(min_lon, max_lon), lat=slice(min_lat, max_lat))
+    # calculate temporal average across the time axis only
+    mean_array = x.mean(dim='time')
+    # print(mean_array)
+    return mean_array
 
 
 class X(unittest.TestCase):
 
-    def test_get_years(self):
-        file1 = 'badc/cmip5/data/cmip5/output1/MOHC/HadCM3/rcp45/mon/atmos/Amon/r1i1p1/latest/tas/tas_Amon_HadGEM2-ES_rcp45_r1i1p1_205512-208011.nc'
-        file2 = 'badc/cmip5/data/cmip5/output1/MOHC/HadCM3/rcp45/mon/atmos/Amon/r1i1p1/latest/tas/tas_Amon_HadGEM2-ES_rcp45_r1i1p1_200512-203011.nc'
-        file_path1 = os.path.join(os.environ.get('HOME'), file1)
-        file_path2 = os.path.join(os.environ.get('HOME'), file2)
+    # def test_get_years(self):
+    #     file1 = 'badc/cmip5/data/cmip5/output1/MOHC/HadCM3/rcp45/mon/atmos/Amon/r1i1p1/latest/tas/tas_Amon_HadGEM2-ES_rcp45_r1i1p1_205512-208011.nc'
+    #     file2 = 'badc/cmip5/data/cmip5/output1/MOHC/HadCM3/rcp45/mon/atmos/Amon/r1i1p1/latest/tas/tas_Amon_HadGEM2-ES_rcp45_r1i1p1_200512-203011.nc'
+    #     file_path1 = os.path.join(os.environ.get('HOME'), file1)
+    #     file_path2 = os.path.join(os.environ.get('HOME'), file2)
+    #
+    #     years_none = x.get_years(file_path1)
+    #     self.assertIsNone(years_none)
+    #
+    #     years, years_desired = x.get_years(file_path2)
+    #     self.assertIsNotNone(years, years_desired)
+    #     self.assertTrue(len(years) > len(years_desired))
+    #
+    #
+    #
+    # def test_couple_subset(self):
+    #     file = 'badc/cmip5/data/cmip5/output1/MOHC/HadCM3/rcp45/mon/atmos/Amon/r1i1p1/latest/tas/*.nc'
+    #     file_path = os.path.join(os.environ.get('HOME'), file)
+    #     files = glob.glob(file_path)
+    #     result = x.couple_subset(files)
+    #     self.assertIsNotNone(result)
 
-        years_none = x.get_years(file_path1)
-        self.assertIsNone(years_none)
-
-        years, years_desired = x.get_years(file_path2)
-        self.assertIsNotNone(years, years_desired)
-        self.assertTrue(len(years) > len(years_desired))
-
-    def test_couple_subset(self):
-        file = 'badc/cmip5/data/cmip5/output1/MOHC/HadCM3/rcp45/mon/atmos/Amon/r1i1p1/latest/tas/*.nc'
-        file_path = os.path.join(os.environ.get('HOME'), file)
-        files = glob.glob(file_path)
-        result = x.couple_subset(files)
-        self.assertIsNotNone(result)
+    # def test_open_mfdatasets(self):
+    #     file = 'badc/cmip5/data/cmip5/output1/MOHC/HadCM3/rcp45/mon/atmos/Amon/r1i1p1/latest/tas/*.nc'
+    #     file_path = os.path.join(os.environ.get('HOME'), file)
+    #     files = glob.glob(file_path)
+    #
+    #     files_to_open = x.couple_subset(files)
+    #     result = x.open_mfdatasets(files_to_open)
+    #     self.assertIsNotNone(result)
 
     def test_open_mfdatasets(self):
-        file = 'badc/cmip5/data/cmip5/output1/MOHC/HadCM3/rcp45/mon/atmos/Amon/r1i1p1/latest/tas/*.nc'
-        file_path = os.path.join(os.environ.get('HOME'), file)
-        files = glob.glob(file_path)
+        """
+            Test to calculate temporal average across the time axis only
+        """
+        self.assertIsNotNone(calc_temporal_avg_one())
 
-        files_to_open = x.couple_subset(files)
-        result = x.open_mfdatasets(files_to_open)
-        self.assertIsNotNone(result)
+    def test_error(self):
+        """
+            Test if calc_temporal_avg() throws a KeyError when
+            calculating temporal average on aggregated .nc files
+            ERROR - KeyError: cftime.Datetime360Day(2010-01-01 00:00:00)
+            when Xarray's function "open_mfdataset" does not include
+            combine='by_coords'
+        """
+        with self.assertRaises(KeyError): calc_temporal_avg_multiple()
 
 
 if __name__ == '__name__':

xarray1.py
@@ -1,33 +1,41 @@
-from x import get_years, couple_subset, open_mfdatasets
+from x import get_years, open_mfdatasets
 import glob
 import time
 import os
 import cftime
 from pathlib import Path
-
-
+import pandas as pd
+import xarray as xr
 start_time = time.time()
 max_lon, min_lon, max_lat, min_lat = 120, 80, 60, -60
-
-# one_file = 'tas_Amon_HadGEM2-ES_rcp45_r1i1p1_203012-205511.nc'
 files_path = 'badc/cmip5/data/cmip5/output1/MOHC/HadCM3/rcp45/mon/atmos/Amon/r1i1p1/latest/tas'
 absolute_path = os.path.join(str(Path.home()), files_path)
 files = glob.glob(absolute_path + '/*.nc')
 # #
-# d1 = cftime.Datetime360Day(2010, 1, 1)
-# d2 = cftime.Datetime360Day(2020, 1, 1)
+
 
 
 if __name__=='__main__':
-    print(get_years(files))
-
-    # files_to_open = couple_subset(files)
-    # new_dataset = open_mfdatasets(files_to_open)
-    #
-    # x = new_dataset['tas'].sel(time=slice(d1, d2), lon=slice(min_lon, max_lon), lat=slice(min_lat, max_lat))
-    # # calculate temporal average across the time axis only
-    # mean_array = x.mean(dim='time')
-    # for i in mean_array:
+    d1 = cftime.Datetime360Day(2005, 1, 1)
+    d2 = cftime.Datetime360Day(2100, 1, 1)
+    # files_to_work_with = get_years(files)
+    y = open_mfdatasets(files)
+    # print(type(y))
+    # print(len(y))
+    # print(y)
+    # print(y)
+    # files_to_open = couple_subset(files_to_work_with)
+    # print(files_to_open)
+    # new_dataset = open_mfdatasets(files_to_open[5])
+    # print(new_dataset.variables['time'])
+    x = y.sel(time=slice(d1, d2), lon=slice(min_lon, max_lon), lat=slice(min_lat, max_lat))
+    # calculate temporal average across the time axis only
+    mean_array = x.mean(dim='time')
+    print(mean_array)
+    mean_array.to_netcdf('result.nc')
+    # print(mean_array.tas[0,0])
+    # print(mean_array)
+    # for i in x:
     #     print(i)
 
     print(f'took {time.time() - start_time} seconds')